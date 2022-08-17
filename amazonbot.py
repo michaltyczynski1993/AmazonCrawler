3f29129d27014f633a89efc755e78745e96917e6
Michal Tyczynski <m.tyczynski1993@gmail.com>
2022-08-17 10:33:35 +0200

    Locator bug fix and data extraction from 1st page

--- amazonbot.py
+++ amazonbot.py
@@ -1,8 +1,8 @@
-from multiprocessing.resource_sharer import stop
-from tracemalloc import start
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import timeit
 
 # driver init
@@ -21,18 +21,27 @@ search_input.send_keys('iphone xr')
 search_button = driver.find_element(By.ID, 'nav-search-submit-button')
 search_button.click()
 
-results = driver.find_elements(By.XPATH, '//*[@data-component-type = "s-search-result"]')
+def locate_elements():
+    results = WebDriverWait(driver,10).until(
+        EC.presence_of_all_elements_located((By.XPATH, '//div[contains(@class, "s-result-item s-asin")]')))
+    for result in results:
+        title = result.find_element(By.XPATH, './/h2[contains(@class, "a-color-base")]')
+        price = result.find_element(By.XPATH, './/*[@class = "a-price"]')
+        link = result.find_element(By.XPATH, './/*[contains(@class, "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal")]').get_attribute("href")
 
-for result in results:
-    title = driver.find_element(By.XPATH, '//*[contains(@class, "a-size-base-plus a-color-base a-text-normal")]').text
-    price = driver.find_element(By.XPATH, '//*[@class = "a-price"]').text
-    link = driver.find_element(By.XPATH, '//*[contains(@class, "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal")]').get_attribute('href')
 
-    print(title)
-    print(price)
-    print(link)
+        print(title.text)
+        print(price.text)
+        print(link)
+
+
+number_of_pages = int(driver.find_element(By.XPATH, '//*[contains(@class, "s-pagination-item s-pagination-disabled")]').text)
+
+# while number_of_pages -1:
+locate_elements()
+    # next_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[text() = "Dalej"]')))
+    # next_button.click()
+    # number_of_pages -= 1
 
 stop = timeit.default_timer()
 print('Time: ', stop - start)
-
-
