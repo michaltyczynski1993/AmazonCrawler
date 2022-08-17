41eb2c16c910ece4adb6ebc2f0eb97d791575684
Michal Tyczynski <m.tyczynski1993@gmail.com>
2022-08-17 08:12:55 +0200

    Element locators and extract data from Amazon page

--- amazonbot.py
+++ amazonbot.py
@@ -1,10 +1,14 @@
+from multiprocessing.resource_sharer import stop
+from tracemalloc import start
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import timeit
 
 # driver init
 PATH= 'C:\TestFiles\chromedriver.exe'
 driver = webdriver.Chrome(executable_path=PATH)
+start = timeit.default_timer()
 driver.get("https://www.amazon.pl/")
 
 #close cookie
@@ -16,4 +20,19 @@ search_input = driver.find_element(By.ID, 'twotabsearchtextbox')
 search_input.send_keys('iphone xr')
 search_button = driver.find_element(By.ID, 'nav-search-submit-button')
 search_button.click()
+
+results = driver.find_elements(By.XPATH, '//*[@data-component-type = "s-search-result"]')
+
+for result in results:
+    title = driver.find_element(By.XPATH, '//*[contains(@class, "a-size-base-plus a-color-base a-text-normal")]').text
+    price = driver.find_element(By.XPATH, '//*[@class = "a-price"]').text
+    link = driver.find_element(By.XPATH, '//*[contains(@class, "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal")]').get_attribute('href')
+
+    print(title)
+    print(price)
+    print(link)
+
+stop = timeit.default_timer()
+print('Time: ', stop - start)
+
 
