f3203620e8747e8067da35e05a9e8ba1d85c9da5
Michal Tyczynski <m.tyczynski1993@gmail.com>
2022-08-19 08:44:11 +0200

    Locating items on whole pages - bug fixed

--- amazonbot.py
+++ amazonbot.py
@@ -4,6 +4,38 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import timeit
+import time
+import pandas as pd
+
+products_title = []
+products_price = []
+products_link = []
+templist = []
+
+def locate_elements():
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    results = WebDriverWait(driver,10).until(
+        EC.presence_of_all_elements_located((By.XPATH, '//div[contains(@class, "s-result-item s-asin")]')))
+    for result in results:
+        try:
+            title = result.find_element(By.XPATH, './/h2[contains(@class, "a-color-base")]')
+            price = result.find_element(By.XPATH, './/*[@class = "a-price"]')
+            link = result.find_element(By.XPATH, './/*[contains(@class, "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal")]').get_attribute("href")
+        except:
+            title = 'NULL'
+            price = 'NULL'
+            link = 'NULL'
+            
+        try:
+            Table_dict={ 'title': title.text,
+                    'price': price.text,
+                    'link': link}
+            
+            templist.append(Table_dict)
+        except:
+            pass
+
 
 # driver init
 PATH= 'C:\TestFiles\chromedriver.exe'
@@ -24,27 +56,17 @@ search_input.send_keys('iphone xr')
 search_button = driver.find_element(By.ID, 'nav-search-submit-button')
 search_button.click()
 
-def locate_elements():
-    results = WebDriverWait(driver,10).until(
-        EC.presence_of_all_elements_located((By.XPATH, '//div[contains(@class, "s-result-item s-asin")]')))
-    for result in results:
-        title = result.find_element(By.XPATH, './/h2[contains(@class, "a-color-base")]')
-        price = result.find_element(By.XPATH, './/*[@class = "a-price"]')
-        link = result.find_element(By.XPATH, './/*[contains(@class, "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal")]').get_attribute("href")
-
-
-        print(title.text)
-        print(price.text)
-        print(link)
-
-
 number_of_pages = int(driver.find_element(By.XPATH, '//*[contains(@class, "s-pagination-item s-pagination-disabled")]').text)
-
-while number_of_pages -1:
+while number_of_pages >= 0:
     locate_elements()
-    next_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[text() = "Dalej"]')))
+    next_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[text() = "Dalej"]')))
     next_button.click()
-    number_of_pages -= 1
+    number_of_pages -=1
+
+
+df = pd.DataFrame(templist)
+df.to_csv('table.csv') 
+# driver.close()
 
 stop = timeit.default_timer()
 print('Time: ', stop - start)
